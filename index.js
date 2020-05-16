5ce83b002603ebc2fedb6a5fa9e27fda65f5d6d8
blessed-publisher-8631 <jhonny.chamoun@gmail.com>
2020-05-16 21:23:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,12 +19,10 @@ function currentLine(line)
   if(line.length===0)
     return "The line is currently empty."
   else
-    for(let i=0; i<line.length; i++)
+    for(let i=0; i<line.length-1; i++)
     {
-      res+=i+1+". "+line[i]
-      if(i<line.length-1){
-        res+=", "
-      }
+      res+=i+1+". "+line[i]+", "
     }
+    res+=
   return res
 }
