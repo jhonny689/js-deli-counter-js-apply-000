dfaf2b5101762cb72e6bd96a8a0aefcc597e0095
blessed-publisher-8631 <jhonny.chamoun@gmail.com>
2020-05-16 21:02:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function takeANumber(line, name)
   line.push(name)
   return "Welcome, "+name+". You are number "+line.length+" in line."
 }
+
+function nowServing(line)
+{
+  var currentlyServed = line.pop()
+}
